4f1454c0aa098947f5b01396cf7335c2e120d81d
sforer580 <sforer580@gmail.com>
2017-02-12 12:53:05 -0800

    y2 and y2 dot functions
    Added the necessary calculations for the y2 and y2 dot functions.

--- Simulator.hpp
+++ Simulator.hpp
@@ -38,12 +38,13 @@ protected:
 public:
     Parameters* pP;
     void Initialize_Agent(Individual* pI);
-    void Get_New_y1(int t, Individual* pI);
-    void Get_New_y1_d(int t, Individual* pI);
-    void Get_New_y2(int t, Individual* pI);
-    void Get_New_y2_d(int t, Individual* pI);
-    void Get_New_Accel(int t, Individual* pI);
-    void Run_Time_Step(int t, Individual* pI);
+    void Get_New_x(double t, int ts, Individual* pI);
+    void Get_New_y2(double t, int ts, Individual* pI);
+    void Get_New_y2_d(double t, int ts, Individual* pI);
+    void Get_New_Accel(double t, int ts, Individual* pI);
+    void Get_New_y1(double t, int ts, Individual* pI);
+    void Get_New_y1_d(double t, int ts, Individual* pI);
+    void Run_Time_Step(double t, int ts, Individual* pI);
     void Simulate(Individual* PI);
     
     
@@ -69,8 +70,15 @@ void Simulator::Initialize_Agent(Individual* pI)
 
 
 //-------------------------------------------------------------------------
+//Gets the new x
+void Simulator::Get_New_x(double t, int ts, Individual* pI)
+{
+    pI->x.push_back(pI->x.at(ts-1)+pP->delta_t);
+}
+
+//-------------------------------------------------------------------------
 //Gets the new acceleration
-void Simulator::Get_New_Accel(int t, Individual* pI)
+void Simulator::Get_New_Accel(double t, int ts, Individual* pI)
 {
     
 }
@@ -78,7 +86,7 @@ void Simulator::Get_New_Accel(int t, Individual* pI)
 
 //-------------------------------------------------------------------------
 //Gets the new y1
-void Simulator::Get_New_y1(int t, Individual* pI)
+void Simulator::Get_New_y1(double t, int ts, Individual* pI)
 {
     
 }
@@ -86,7 +94,7 @@ void Simulator::Get_New_y1(int t, Individual* pI)
 
 //-------------------------------------------------------------------------
 //Gets the new y1 dot
-void Simulator::Get_New_y1_d(int t, Individual* pI)
+void Simulator::Get_New_y1_d(double t, int ts, Individual* pI)
 {
     
 }
@@ -94,29 +102,39 @@ void Simulator::Get_New_y1_d(int t, Individual* pI)
 
 //-------------------------------------------------------------------------
 //Gets the new y2
-void Simulator::Get_New_y2(int t, Individual* pI)
+void Simulator::Get_New_y2(double t, int ts, Individual* pI)
 {
-    
+    int pi = 3.14159;
+    double A = pP->amp*sin((2*pi)/(pP->travel_speed*pP->period));
+    double B = pI->x.at(ts-1);
+    double C = pP->travel_speed*pP->delta_t;
+    double D = pP->travel_speed*t;
+    pI->y2.push_back(A*(B+C-D));
 }
 
 
 //-------------------------------------------------------------------------
 //Gets the new y2 dot
-void Simulator::Get_New_y2_d(int t, Individual* pI)
+void Simulator::Get_New_y2_d(double t, int ts, Individual* pI)
 {
-    
+    int pi = 3.14159;
+    double A = pP->omega*pP->amp*cos((2*pi)/(pP->lamda));
+    double B = pI->x.at(ts);
+    double C = pP->travel_speed*t;
+    pI->y2_d.push_back(A*(B+C));
 }
 
 
 //-------------------------------------------------------------------------
 //Runs a single time step
-void Simulator::Run_Time_Step(int t, Individual* pI)
+void Simulator::Run_Time_Step(double t, int ts, Individual* pI)
 {
-    Get_New_Accel(t, pI);
-    Get_New_y1(t, pI);
-    Get_New_y1_d(t, pI);
-    Get_New_y2(t, pI);
-    Get_New_y2_d(t, pI);
+    Get_New_x(t, ts, pI);
+    Get_New_y2(t, ts, pI);
+    Get_New_y2_d(t, ts, pI);
+    Get_New_Accel(t, ts, pI);
+    Get_New_y1(t, ts, pI);
+    Get_New_y1_d(t, ts, pI);
 }
 
 
@@ -124,7 +142,9 @@ void Simulator::Run_Time_Step(int t, Individual* pI)
 //Runs the entire simulation process
 void Simulator::Simulate(Individual* pI)
 {
-    for (int t=0; t<pP->time_max; t++)
+    double t = 0;
+    int ts = 0;
+    while (t < pP->time_max)
     {
         if (t == 0)
         {
@@ -132,8 +152,10 @@ void Simulator::Simulate(Individual* pI)
         }
         if (t > 0)
         {
-            Run_Time_Step(t,pI);
+            Run_Time_Step(t, ts, pI);
         }
+        ts += 1;
+        t += pP->delta_t;
     }
     //cout << "IN" << "\t" << pI->K1 << endl;
     //pI->K1 = 1;
